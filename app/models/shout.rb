cf0dad50720e1be1ef8659723304a9069cbd5800
Ana Paola Treviño <anapao.trevino@gmail.com>
2019-01-23 14:47:03 -0600

    user shout page

--- app/models/shout.rb
+++ app/models/shout.rb
@@ -1,4 +1,10 @@
 class Shout < ApplicationRecord
+  belongs_to :user
+  
   validates :user, presence: true
   validates :body, presence: true, length: { in: 1..144 }
+
+  default_scope { order(created_at: :desc) }
+
+  delegate :username, to: :user
 end
